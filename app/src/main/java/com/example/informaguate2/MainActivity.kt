0969af96539f050a78b23f3354d837af7bf9b8ee
Raúl Angel Jiménez <Raulangel51@users.noreply.github.com>
2020-02-02 20:06:25 -0600

    Initial commit

---
+++ app/src/main/java/com/example/informaguate2/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.informaguate2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mostrar: TextView;
+    lateinit var text: TextView;
+    lateinit var  nombre: EditText;
+    lateinit var intento: Intent;
+    var cont = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //variables para referenciar los textview
+        mostrar =findViewById(R.id.textView_mostrar)
+        text = findViewById(R.id.textView_nombre)
+        nombre = findViewById(R.id.editText_pedir_nombre)
+    }
+
+    fun nombre(view: View){
+        if(cont == 0){
+            //se le coloca el nombre y se hace vivisble
+            mostrar.setText(nombre.text)
+            mostrar.setVisibility(View.VISIBLE)
+
+            //se vuelven quitan los otros textview de la pantalla
+            text.setVisibility(View.GONE)
+            nombre.setVisibility(View.GONE)
+            cont++
+        }
+        else{
+            //se regresa a la normalidad
+            mostrar.setVisibility(View.GONE)
+            text.setVisibility(View.VISIBLE)
+            nombre.setVisibility(View.VISIBLE)
+            nombre.setText("")
+            cont=0;
+        }
+    }
+
+    fun AntiguaInfo(view: View){
+        intento = Intent(this,antigua_information::class.java);
+        startActivity(intento);
+    }
+
+    fun Tikal(view: View){
+        intento = Intent(this, tikal_information::class.java)
+        startActivity(intento);
+    }
+
+    fun Reu(view: View){
+        intento = Intent(this,reu_information::class.java)
+        startActivity(intento);
+    }
+}
---
+++ app/src/main/java/com/example/informaguate2/tikal_information.kt
@@ -0,0 +1,30 @@
+package com.example.informaguate2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class tikal_information : AppCompatActivity() {
+
+    lateinit var  comentario: EditText;
+    lateinit var intento: Intent;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tikal_information)
+
+        comentario = findViewById(R.id.coment)
+    }
+
+    fun regresar(view: View){
+        intento = Intent(this, MainActivity::class.java);
+        startActivity(intento);
+
+        val toast = Toast.makeText(applicationContext, comentario.text, Toast.LENGTH_SHORT)
+        toast.show();
+
+    }
+}
